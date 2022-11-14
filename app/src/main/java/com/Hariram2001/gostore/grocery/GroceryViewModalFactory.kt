233a9a44c7c892a15cae3cb812a6afcdb22606f5
Hariram B <93965734+Hariram2001@users.noreply.github.com>
2022-11-14 20:55:10 +0530

    Update GroceryViewModalFactory.kt

--- app/src/main/java/com/Hariram2001/gostore/grocery/GroceryViewModalFactory.kt
+++ app/src/main/java/com/Hariram2001/gostore/grocery/GroceryViewModalFactory.kt
@@ -1,8 +1,8 @@
-package com.noobshubham.gostore.grocery
+package com.Hariram2001.gostore.grocery
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.noobshubham.gostore.database.GroceryRepository
+import com.Hariram2001.gostore.database.GroceryRepository
 
 class GroceryViewModalFactory(private val repository: GroceryRepository) :
     ViewModelProvider.NewInstanceFactory() {
